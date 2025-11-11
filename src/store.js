e0df9209f024bd014fe02d2463cd066a3970dfb7
Maroko Gideon <marokogideon@gmail.com>
2025-11-11 18:31:20 +0300

    fix toggle not found when page reloads

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
+import { Task } from "./Task.js";
 
 const STORAGE_KEY = "5UP3R5CR3T-2305";
 
 // Load tasks
 export function loadTasks() {
-  const data = localStorage.getItem(STORAGE_KEY);
-  return data ? JSON.parse(data) : [];
+  const raw = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+  return raw.map(t => new Task(t.id, t.title, t.done, t.dueDate, t.category));
 }
-
 // Save tasks
 export function saveTasks(tasks) {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
